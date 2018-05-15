bba32d7f39c642416bfb82ddd77a694585b73cde
Eben Marks <25953206+ebenmarks@users.noreply.github.com>
2018-05-15 10:39:02 +0200

    Update ppex5.py
    Refactored to simplify code by removing repetitive steps. Redundant comments updated or removed.

--- ppex5.py
+++ ppex5.py
@@ -1,25 +1,16 @@
-#create two sample lists
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 17]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17]
 
-#make a variable of the the highest number in both lists
-highest = max([max(a), max(b)])
-
-#create list from 1 to the highest number to iterate through
-lsrng = range(1, highest)
-
-#create empty list for the results
 results = []
 
-#define a function to check if number is in both lists
-def comp(n):
-    if n in a and n in b:
-        #add the number to the empty list
+def look_in_b(n):
+    #We want to know if a number from list a is in list b
+    if n in b:
+        #then we want to save the results so we can print them later
         results.append(n)
 
-#iterate through the lists
-for i in lsrng:
-    comp(i)
+#now we need to run each number in a through the look_in_b function
+for i in a:
+    look_in_b(i)
 
-#print the results
 print(results)
