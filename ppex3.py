901d010326c985682e755085283a07cbb8d7f2e1
Eben Marks <25953206+ebenmarks@users.noreply.github.com>
2018-05-16 12:51:09 +0200

    Updated list name

--- ppex3.py
+++ ppex3.py
@@ -1,9 +1,9 @@
-all = [1, 1, 2, 11, 5, 5, 5, 6, 7, 9, 9, 10, 11, 12, 14, 18, 23]
+more = [1, 1, 2, 11, 5, 5, 5, 6, 7, 9, 9, 10, 11, 12, 14, 18, 23]
 less = []
 
 check = int(input("Give me a number: "))
 
-for n in all:
+for n in more:
     if n < check:
         less.append(n)
 
